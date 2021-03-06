36d5518ab9bd349d406fa66603918b315217bf62
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-03-06 11:43:17 +0100

    Supress folder

--- python translations/subroutine trajlimtest.py
+++
@@ -1,4 +0,0 @@
-def trajlimtest(nframe, MAXFRAMES):
-	if (nframe > MAXFRAMES):
-		print(' Calculation stopped since this option is limited to reading  frames Recompile with larger value of the parameter MAXFRAMES')
-		
