9768c3fea796f1ae45eb060e236ce37be4e88881
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-03-06 15:49:30 +0100

    Add 15:49 06/03

--- fortran to python conversion/dependencies/subroutine dtqli.py
+++ fortran to python conversion/dependencies/subroutine dtqli.py
@@ -5,8 +5,8 @@ def dtqli(d,e,n,np,z,ierr):
 	
 	***
 	Parameters:
-	d: vector of dimension np
-	e: vector of dimension np
+	d: diagonal of dimension np
+	e: offdiagonal of dimension np
 	z: matrix of dimension np*np
 	n:
 	np:
@@ -28,7 +28,7 @@ def dtqli(d,e,n,np,z,ierr):
 		for i in range(n):
 			iter = 0
 			for m in range(n-1):
-				dd = abs(d[m] + abs(d[m+1])
+				dd = abs(d[m]) + abs(d[m+1])
 				# if abs(e[m]) + dd == dd fo to 2
 				# MM 10/21/2004
 				#if (e[m] == 0) fo to 2
