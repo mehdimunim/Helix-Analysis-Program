74ebaf653e09f574c14cdd9f38887a7bc7bb3f47
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-03-17 09:28:48 +0100

    Call_tree worked at last

--- subroutines/call-tree-visualization/call_tree.py
+++ subroutines/call-tree-visualization/call_tree.py
@@ -9,18 +9,19 @@ import os
 class Call_tree:
 
     def __init__(self, name):
-        # dictionary with the name of the subroutine and the number of occurences
         self.name = name
-        self.name_and_occurences = {name: 1}
-        self.children = []
+        # dictionary with children and number of calls
+        self.children = {}
 
     def get_subroutines_call(self):
         """
         Get all subroutines list from a file
         """
+        os.chdir(
+            "C:\\Users\\Mehdi\\Documents\\GitHub\\Interdisciplinary-Project\\subroutines\\all")
         # Exclude the main file simulaid.f
         if (self.name != "simulaid"):
-            file = "subroutine " + self.name
+            file = "subroutine " + self.name.strip()
         file += ".f"
         res = []
         if (os.path.isfile(file)):
@@ -32,51 +33,54 @@ class Call_tree:
                     if (line.strip().startswith("call")):
                         name = line.strip().split('(')[0][len("call"):]
                         res.append(name)
+        else:
+            print(file, "not found")
         return res
 
-    def __eq__(self, tree):
-        return self.name == tree.name
+    def __eq__(self, other):
+        return self.name == str(other)
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __str__(self):
+        return (self.name)
 
     def fill(self):
         """
-
         Fills the call tree recursively
-
-
         """
         # get every calls in the file
         subroutines = self.get_subroutines_call()
         # check if the node is orphane
         if (len(subroutines) != 0):
             for sub in subroutines:
-                sub_is_child = False
-                # adding one call else appending the new subroutine
-                for child in self.children:
-                    if sub == child.name:
-                        child.name_and_occurences[sub] += 1
-                        sub_is_child = True
-                        break
-                if not sub_is_child:
+                if sub in self.children:
+                    self.children[sub] += 1
+                else:
                     tree = Call_tree(sub)
                     tree.fill()
-                    self.children.append(tree)
+                    self.children.update({tree: 1})
 
 
 def pprint_tree(node, file=None, _prefix="", _last=True):
     print(_prefix, "`- " if _last else "|- ", node.name,
-          len(node.children), sep="", file=file)
+          len(node.children.values()), sep="", file=file)
     _prefix += "   " if _last else "|  "
     child_count = len(node.children)
-    for i, child in enumerate(node.children):
+    for i, child in enumerate(node.children.keys()):
         _last = i == (child_count - 1)
         pprint_tree(child, file, _prefix, _last)
 
 
+def print_tree(tree):
+    return None
+
+
 def main():
-    main_tree = Call_tree("analyze")
+    main_tree = Call_tree("dssp")
     main_tree.fill()
     pprint_tree(main_tree)
-    print(len(main_tree.children))
 
 
 main()
