20cefd9846249b5cdcbec27b7d09d5ce4181185f
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-04-26 16:38:50 +0200

    Update tpr (turn per residue)

---
+++ coding by ourselves/TPR/tpr.py
@@ -0,0 +1,13 @@
+#
+# Turn per residue
+#
+
+
+def tpr(**kwargs):
+    """
+    Calculate the average turn angle per residue;
+    Inspired from TRAJELIX from simulaid.
+    (Mezei, Filizola, 2006)
+    """
+
+    
