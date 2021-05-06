9577ad39c7440b8579546ca79cf2c4c982f409f5
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-05-06 21:33:59 +0200

    Create distance.py

--- src/DSSP/hbonds.py
+++ src/DSSP/hbonds.py
@@ -2,15 +2,7 @@
 # Finds the H-bonds
 #
 
-import math
-
-
-def r(atom1, atom2):
-    """
-    Calculates distance between atom1 and atom2
-    atom1, atom2 are 3-arrays of coordinates
-    """
-    return math.sqrt((atom1[0] - atom2[0])**2 + (atom1[1] - atom2[1])**2 + (atom1[2] - atom2[2])**2)
+from distance import distance as r
 
 
 def adapt_hbond(hbond, res_list):
