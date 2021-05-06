988d36c91ff67dc1a3440d9e29b3a0b4882bf5e1
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-05-06 17:33:36 +0200

    Update normal computation

--- coding by ourselves/TPR/normal.py
+++ coding by ourselves/TPR/normal.py
@@ -1,24 +1,36 @@
 import numpy as np
 
 
-def normal(axis, axis_origin, point):
+def normal(axis_direction, axis_origin, point):
     """
     Calculate the normal to axis intercepting point
     ---
     Parameters:
-    axis 
+    axis_direction: normal_direction of the axis
     axis_origin: starting point of the axis
-    point
+    point: intercepts the normal
 
     ---
     Return:
-    direction: steering vector of normal 
-    origin: starting point of normal
+    normal_direction: steering vector of normal 
+    normal_origin: starting point of normal
 
     """
-    
+    # vector from axis normal_origin to CA
+    vector = point - axis_origin
 
+    # projection of vector on axis
+    projection = np.dot(vector, axis)*axis_direction
 
+    # the normal vector from normal_origin to point
+    normal_direction = projection - vector
 
+    # normalized normal_direction
+    normal_direction = normal_direction/np.linalg.norm(normal_direction)
 
-    return direction, origin
+    # opposite of the projection of vector on normal 
+    projection2 = - np.dot(normal_direction, vector)*normal_direction
+
+    normal_origin = point + projection2
+
+    return normal_direction, normal_origin
