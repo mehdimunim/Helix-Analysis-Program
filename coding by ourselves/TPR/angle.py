b107a5a38161a2400209047326fc4b7e39c338cc
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-05-06 18:21:54 +0200

    Update TPR (not tested yet)

--- coding by ourselves/TPR/angle.py
+++ coding by ourselves/TPR/angle.py
@@ -1,8 +1,12 @@
+from numpy import dot
+from numpy.linalg import norm
+from math import arccos
 
-def angle(v1, v2, reference):
+
+def angle(v1, v2):
     """
     Calculate the angle between v1 and v2.
     """
-    angle = dot(v1, v2)/(norm(v1)*norm(v2))
-    sign = sign(cross(v1, reference))
-    return sign*angle
+    cos_angle = dot(v1, v2)/(norm(v1)*norm(v2))
+    angle = arccos(cos_angle)
+    return angle
--- coding by ourselves/TPR/tpr.py
+++ coding by ourselves/TPR/tpr.py
@@ -4,7 +4,7 @@
 
 from normal import normal
 from angle import angle
-from regression import regression
+from fit_angles import fit_angles
 
 
 def tpr(alpha_carbons, axis):
@@ -14,9 +14,9 @@ def tpr(alpha_carbons, axis):
 
     Two steps:
 
-    1- Calculate the turn angles phi(i) between first residue and i
+    1- Calculate the turn angles phi_i between first residue and i
 
-    2- Fit ph(i) = theta*i + phi_0 where theta is the turn angle per residue
+    2- Fit phi_i = theta*i + phi_0 where theta is the turn angle per residue
 
     (Mezei, Filizola, 2006)
 
@@ -36,10 +36,10 @@ def tpr(alpha_carbons, axis):
 
     phi = []
     phi_i = 0
-    for i in range(3, n):
+    for pos in range(1, len(alpha_carbons)):
         # calculate normal vectors for residue i-1 and i
-        vec_before = normal(axis, alpha_carbons[i-1])
-        vec_after = normal(axis, alpha_carbons[i])
+        vec_before, _ = normal(axis, alpha_carbons[i-1])
+        vec_after, _ = normal(axis, alpha_carbons[i])
 
         # angle between i-1 and i
         angle = angle(vec_before, vec_after)
@@ -52,6 +52,6 @@ def tpr(alpha_carbons, axis):
     # Second step
     # Find turn angle per residue with linear regression
 
-    theta, phi_0 = linear_regression(phi)
+    theta, phi_0 = fit_angles(phi)
 
     return theta
