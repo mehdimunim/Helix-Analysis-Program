f4f6914ef78f75f11bdad021b1f8cd1a4f490b7e
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-05-09 12:11:13 +0200

    Update parse

--- src/Parser/parse.py
+++ src/Parser/parse.py
@@ -2,50 +2,14 @@
 # Gets the backbone atoms
 #
 
-
-def parse(filename):
-    """
-    Parse a pdb file to get all backbone atoms
-
-    ---
-    Parameters:
-    filename: trajectory or static pdb file
-
-    ---
-    Returns:
-    if trajectory, list of backbones for each frames
-    else, backbone of the molecule
-
-    """
-    limits = get_frames_limits(filename)
-    backbones = []
-    for limit in limits:
-        backbone = parse_structure(filename, limit[0], limit[1])
-        backbones.append(backbone)
-    if (len(backbones) == 1):
-        return backbones[0]
-    return backbones
-
-
-def parse_structure(filename, start, end):
+def parse_structure(filename):
     """
     Parse PDB entry file
     to get the CA, C, O, N and H from the backbone chain
-
+    Returns: the corresponding coordinates for each residue
     ---
-    Parameters:
-    filename: pdb input file (trajectory or static)
-    start: line from which to start reading the file
-    end: line from which to stop reading
-
-    ---
-    Returns:
-    backbone: the corresponding coordinates for each residue
-
-    ---
-    NOTE: Supress first and last residues
+    NOTE: Supress first and last residues as they are electrically charged
     """
-
     alpha_carbons = []
     simple_carbons = []
     oxygens = []
@@ -53,10 +17,9 @@ def parse_structure(filename, start, end):
     hydrogens = []
     res_number_list = []
 
+    # Itérer sur les lignes dans pdbname
     with open(filename, "r") as pdbname:
-        # Select slice to read
-        lines = pdbname.readlines()[start:end+1]
-        for line in lines:
+        for line in pdbname:
 
             if line.startswith('ATOM'):
 
@@ -116,52 +79,11 @@ def parse_structure(filename, start, end):
     return backbone
 
 
-def get_frames_limits(filename):
-    """
-    Get starts and ends of each frames a trajectory file
-    Get start and end of the file for a static file
-    ---
-    Parameters:
-    filename: trajectory or static pdb file
-
-    ---
-    Return:
-    frames: list of tuple with (startline, endline) for each frame
-
-    """
-    frames = []
-    with open(filename, "r") as pdbname:
-        lines = pdbname.readlines()
-        start = 1
-        end = len(lines)
-        for i, line in enumerate(lines):
-            if line.startswith("MODEL"):
-                start = i
-
-            elif line.startswith("ENDMDL"):
-                end = i
-                frames.append((start, end))
-    return frames
-
-
 def test_parse():
     import os
     os.chdir(
         "C:\\Users\\Mehdi\\Documents\\GitHub\\Interdisciplinary-Project\\resource")
-
-    # Test for static molecule
-    backbone = parse("TSPO_first_frame.pdb")
-    print("#a_carbon: ", len(backbone["alpha_carbon"]))
-    print("#carbon:   ", len(backbone["carbon"]))
-    print("#hydrogen: ", len(backbone["hydrogen"]))
-    print("#nitrogen: ", len(backbone["nitrogen"]))
-    print("#nitrogen: ", len(backbone["oxygen"]))
-    print("#res:      ", len(backbone["res_number_list"]))
-
-    # Test for trajectory file
-    backbones = parse("TSPO_traj.pdb")
-    backbone = backbones[0]
-    print(len(backbones))
+    backbone = parse_structure("TSPO_first_frame.pdb")
     print("#a_carbon: ", len(backbone["alpha_carbon"]))
     print("#carbon:   ", len(backbone["carbon"]))
     print("#hydrogen: ", len(backbone["hydrogen"]))
