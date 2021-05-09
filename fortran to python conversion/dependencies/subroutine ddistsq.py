b6ba33cd2ddf44ca36ac70ccbd82c6754aaff098
Mehdi Munim <68103264+mehdimunim@users.noreply.github.com>
2021-05-09 23:54:26 +0200

    Delete fortran to python conversion directory

--- fortran to python conversion/dependencies/subroutine ddistsq.py
+++
@@ -1,5 +0,0 @@
-def ddistsq(x,x0):
-	""" 
-	returns squared distance between x and x0
-	"""
-	return (x[0] - x0[0])**2 + (x[1]-x0[1])**2 + (x[2] - x0[2])**2
--- fortran to python conversion/dependencies/subroutine trajlimtest.py
+++
@@ -1,4 +0,0 @@
-def trajlimtest(nframe, MAXFRAMES):
-	if (nframe > MAXFRAMES):
-		raise ValueError(' Calculation stopped since this option is limited to reading {} frames Recompile with larger value of the parameter MAXFRAMES'.format(MAXFRAMES))
-		
